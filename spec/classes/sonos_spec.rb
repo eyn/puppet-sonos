84739fefb5e6acd517596978dc14f2e3f7a04152
James Moore <james.moore@red-gate.com>
2014-04-27 19:53:54 +0100

    Update to 4.3

--- spec/classes/sonos_spec.rb
+++ spec/classes/sonos_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe 'sonos' do
   it do
-    should contain_package('SonosDesktopController42').with({
+    should contain_package('SonosDesktopController43').with({
       :provider => 'appdmg_eula',
-      :source   => 'http://update.sonos.com/software/mac/mdcr/SonosDesktopController42.dmg',
+      :source   => 'http://update.sonos.com/software/mac/mdcr/SonosDesktopController43.dmg',
     })
   end
 end
